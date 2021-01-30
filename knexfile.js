91adde73c50706cd6dcbc82cbc341e1afaf2a542
Camilla Maria <camillamaria94@gmail.com>
2021-01-30 12:37:08 -0300

    add PORT env and knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,8 @@ module.exports = {
       host: process.env.DATABASE_HOST,
       database: process.env.DATABASE_NAME,
       user:     process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD
+      password: process.env.DATABASE_PASSWORD,
+      port: process.env.DATABASE_PORT
     },
     pool: {
       min: 2,
