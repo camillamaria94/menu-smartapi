eb2ca43c762c37f5b964c950ccb3069bba6e0f28
Camilla Maria <camillamaria94@gmail.com>
2021-02-08 22:29:50 -0300

    alter knex.js

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,15 @@
 module.exports = {
 
   development: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    ssl: true,
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      port: process.env.DATABASE_PORT,
+      ssl: true
+    },
     pool: {
       min: 2,
       max: 10
