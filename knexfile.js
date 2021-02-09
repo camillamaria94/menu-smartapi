2932339e10c356ebb3a92203d4778b032b225db7
Camilla Maria <camillamaria94@gmail.com>
2021-02-08 22:48:11 -0300

    alter knex.js

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,7 @@ module.exports = {
       user:     process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       port: process.env.DATABASE_PORT,
+      ssl: true
     },
     pool: {
       min: 2,
