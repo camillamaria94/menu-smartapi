901466d96131cceb2751d8a2c0a288f895cb404b
Camilla Maria <camillamaria94@gmail.com>
2021-02-08 21:39:30 -0300

    alter url database

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      host: process.env.DATABASE_HOST,
-      database: process.env.DATABASE_NAME,
-      user:     process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      port: process.env.DATABASE_PORT
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
